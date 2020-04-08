430d8e2694a570389ff5126dc12b2e5a1002e686
Parijat Khan <khanparijat@gmail.com>
2020-04-08 21:34:03 +0530

    Fetch API based on user owned object

--- api/views.py
+++ api/views.py
@@ -5,8 +5,12 @@ from .serializers import ToDoSerializer
 # Create your views here.
 
 class ToDoListCreate(generics.ListCreateAPIView):
-    queryset = ToDo.objects.all()
+    
     serializer_class = ToDoSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['task']
     ordering_fields = ['created_datetime', 'is_complete']
+
+
+    def get_queryset(self):
+        return ToDo.objects.filter(created_by=self.request.user)
